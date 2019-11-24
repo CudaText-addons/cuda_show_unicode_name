e55ea46ceb9bfe1391899fdc13b60751aa0679c6
Alexey-t <support@uvviewsoft.com>
2019-11-25 00:16:20 +0300

    use separate cell

--- __init__.py
+++ __init__.py
@@ -1,16 +1,27 @@
-from cudatext import msg_status as msg
+import cudatext as app
 from unicodedata import name
 
+MYTAG = 101
+
 class Command:
-    
+
+    def __init__(self):
+
+        app.statusbar_proc('main', app.STATUSBAR_ADD_CELL, tag=MYTAG)
+        app.statusbar_proc('main', app.STATUSBAR_SET_CELL_AUTOSIZE, tag=MYTAG, value=True)
+
     def on_caret(self, ed_self):
-        
+
         x, y, x1, y1 = ed_self.get_carets()[0]
         cnt = ed_self.get_line_count()
         if 0<=y<cnt:
             s = ed_self.get_text_line(y, 2000) #limit max len
             if x>=len(s):
-                return msg('')
+                return self.msg('')
             s = s[x]
             s = ('U+%04x'%ord(s)).upper() +', '+ name(s, '?')
-            msg(s)
+            self.msg(s)
+
+    def msg(self, s):
+
+        app.statusbar_proc('main', app.STATUSBAR_SET_CELL_TEXT, tag=MYTAG, value=s)
--- readme/readme.txt
+++ readme/readme.txt
@@ -1,7 +1,7 @@
 plugin for CudaText.
 shows Unicode full name (e.g. "LATIN CAPITAL E") for character under first caret.
-shows it in the statusbar "message" cell (where usual messages show with a delay).
-seems it doesn't slow down editor, on 2010 year average PC.
+shows it in the statusbar in a separate cell, on the right side.
+seems it doesn't slow down editor (on 2010 year average PC).
 
 author: Alexey (CudaText)
 license: MIT
