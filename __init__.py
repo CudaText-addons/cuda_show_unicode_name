36a0a8c7651667b2a5a5b70b95eda897a30c9d7a
Alexey <support@uvviewsoft.com>
2022-09-17 01:39:02 +0300

    global flag-> class var

--- __init__.py
+++ __init__.py
@@ -3,28 +3,21 @@ import cudatext as app
 from unicodedata import name
 
 plugin_name = __name__
-MYTAG = 101
+MYTAG = app.app_proc(app.PROC_GET_UNIQUE_TAG, '')
 BAR_H = app.app_proc(app.PROC_GET_MAIN_STATUSBAR, '')
-enabled = True
 
 fn_config = os.path.join(app.app_path(app.APP_DIR_SETTINGS), 'plugins.ini')
 section = 'show_unicode_name'
 
-
 def bool_to_str(v): return '1' if v else '0'
-
-
 def str_to_bool(s): return s=='1'
 
-
 class Command:
+    enabled = True
 
     def __init__(self):
-        global enabled
-
-        # Read settings if config file exists. By default set to True
         if os.path.isfile(fn_config):
-            enabled = str_to_bool(app.ini_read(fn_config, section, 'enabled', '1'))
+            self.enabled = str_to_bool(app.ini_read(fn_config, section, 'enabled', '1'))
 
         self.set_enabled()
 
@@ -39,7 +32,7 @@ class Command:
         app.statusbar_proc(BAR_H, app.STATUSBAR_SET_CELL_TEXT, tag=MYTAG, value=s)
 
     def set_enabled(self):
-        if enabled:
+        if self.enabled:
             app.statusbar_proc(BAR_H, app.STATUSBAR_ADD_CELL, index=0, tag=MYTAG)
             app.statusbar_proc(BAR_H, app.STATUSBAR_SET_CELL_AUTOSIZE, tag=MYTAG, value=True)
             app.app_proc(app.PROC_SET_EVENTS, plugin_name + ';' + 'on_caret,' + ';;')
@@ -48,10 +41,8 @@ class Command:
             app.statusbar_proc(BAR_H, app.STATUSBAR_DELETE_CELL, tag=MYTAG)
 
     def toggle_display(self):
-        global enabled
-
-        enabled = not enabled
-        app.ini_write(fn_config, section, 'enabled', bool_to_str(enabled))
+        self.enabled = not self.enabled
+        app.ini_write(fn_config, section, 'enabled', bool_to_str(self.enabled))
 
         self.set_enabled()
 
--- readme/readme.txt
+++ readme/readme.txt
@@ -1,7 +1,7 @@
 Plugin for CudaText.
 
-Shows Unicode full name (e.g. "LATIN CAPITAL E") for character under first caret,
-the name is displayed it in the statusbar in a separate cell, on the right side.
+Shows Unicode full name (e.g. "LATIN CAPITAL E") for the character under first caret.
+Name is displayed it in the statusbar in a separate cell, on the left side.
 Testing shows that plugin doesn't slow down the editor (on 2010 year average PC).
 
 Author: Alexey Torgashin (CudaText)
